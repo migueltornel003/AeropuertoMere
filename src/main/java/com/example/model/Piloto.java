eea84e4867a05fc6bb523582763cb2082418316c
migueltornel003 <migueltornel003@gmail.com>
2024-02-15 13:04:07 +0100

    Actualizado la nomenclatura de atributos de Piloto.java

--- src/main/java/com/example/model/Piloto.java
+++ src/main/java/com/example/model/Piloto.java
@@ -1,32 +1,34 @@
 package com.example.model;
 
+import jakarta.persistence.Column;
 import org.hibernate.annotations.NaturalId;
 
 public class Piloto extends Persona{
 
 	@NaturalId
-	private Integer n_lic;
+	@Column(name = "n_lic")
+	private Integer nLic;
 
 	public Piloto() {
 	}
 
 	public Piloto(Long nss, String nombre, String direccion, Integer n_lic) {
 		super(nss, nombre, direccion);
-		this.n_lic = n_lic;
+		this.nLic = n_lic;
 	}
 
 	public Integer getN_lic() {
-		return n_lic;
+		return nLic;
 	}
 
 	public void setN_lic(Integer n_lic) {
-		this.n_lic = n_lic;
+		this.nLic = n_lic;
 	}
 
 	@Override
 	public String toString() {
 		return "Piloto{" +
-				"n_lic=" + n_lic +
+				"n_lic=" + nLic +
 				'}';
 	}
 }
