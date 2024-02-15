648f5b5cc2059837d1826bc364919c0682423b64
migueltornel003 <migueltornel003@gmail.com>
2024-02-15 13:06:10 +0100

    Creada la entidad Tipo.java

--- src/main/java/com/example/model/Tipo.java
+++ src/main/java/com/example/model/Tipo.java
@@ -1,4 +1,59 @@
 package com.example.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Tipo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long modelo;
+
+	private String capacidad;
+
+	private Double peso;
+
+	public Tipo() {
+	}
+
+	public Tipo(Long modelo, String capacidad, Double peso) {
+		this.modelo = modelo;
+		this.capacidad = capacidad;
+		this.peso = peso;
+	}
+
+	public Long getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(Long modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(String capacidad) {
+		this.capacidad = capacidad;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	@Override
+	public String toString() {
+		return "Tipo{" +
+				"modelo=" + modelo +
+				", capacidad='" + capacidad + '\'' +
+				", peso=" + peso +
+				'}';
+	}
 }
