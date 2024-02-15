23bfec326128ef363dbed05cb5b259c8852f0f3a
migueltornel003 <migueltornel003@gmail.com>
2024-02-15 13:00:34 +0100

    Commit de la entidad Hangar.java ha sido actualizado a posibles cambios

--- src/main/java/com/example/model/Hangar.java
+++ src/main/java/com/example/model/Hangar.java
@@ -1,4 +1,57 @@
 package com.example.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Hangar {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "cod_hangar")
+	private Long codHangar;
+
+	private Integer capacidad;
+
+	private String localizacion;
+
+	public Hangar() {
+	}
+
+	public Hangar(Long codHangar, Integer capacidad, String localizacion) {
+		this.codHangar = codHangar;
+		this.capacidad = capacidad;
+		this.localizacion = localizacion;
+	}
+
+	public Long getCodHangar() {
+		return codHangar;
+	}
+
+	public void setCodHangar(Long codHangar) {
+		this.codHangar = codHangar;
+	}
+
+	public Integer getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(Integer capacidad) {
+		this.capacidad = capacidad;
+	}
+
+	public String getLocalizacion() {
+		return localizacion;
+	}
+
+	public void setLocalizacion(String localizacion) {
+		this.localizacion = localizacion;
+	}
+
+	@Override
+	public String toString() {
+		return "Hangar{" +
+				"codHangar=" + codHangar +
+				", capacidad=" + capacidad +
+				", localizacion='" + localizacion + '\'' +
+				'}';
+	}
 }
